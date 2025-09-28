6f8c505ee0e93d04439e4bd4ea3f8ba9cda2981f
Tricky <118668039+RishiRohanKalapala-cloud@users.noreply.github.com>
2025-09-28 19:01:33 +0530

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -7,7 +7,7 @@ export const Testimonials = () => {
     <div className="flex flex-col justify-center items-center text-center text-black py-20 min-h-[300px]">
       <div className="px-5">
         <div className="text-4xl md:text-5xl font-fk-display leading-tight max-w-5xl">
-          Clients can't say enough about Stories at Scale.
+        I always knew content was important, but I never had the time. Stories at Scale made it simple — they pulled stories out of me I didn’t even realize mattered, and the results speak for themselves.
         </div>
       </div>
       
