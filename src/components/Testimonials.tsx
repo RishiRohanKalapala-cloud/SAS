a6944884e0c7fc9b7d0aa3ab3d8785aa1165fe96
Tricky <118668039+RishiRohanKalapala-cloud@users.noreply.github.com>
2025-09-29 00:12:33 +0530

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,15 +4,15 @@ import React from "react";
 
 export const Testimonials = () => {
   return (
-    <div className="flex flex-col justify-center items-center text-center text-black py-20 min-h-[300px]">
+    <div className="flex flex-col justify-center items-center text-left md:text-center text-black py-12 md:py-20 min-h-[300px]">
       <div className="px-5">
-        <div className="text-4xl md:text-5xl font-fk-display leading-tight max-w-5xl">
+        <div className="text-2xl md:text-5xl font-fk-display leading-tight max-w-5xl">
         I always knew content was important, but I never had the time. Stories at Scale made it simple — they pulled stories out of me I didn’t even realize mattered, and the results speak for themselves.
         </div>
       </div>
       
       {/* Centered Quotation Mark */}
-      <div className="mt-12 text-6xl md:text-8xl font-serif text-gray-300">
+      <div className="mt-8 md:mt-12 text-6xl md:text-8xl font-serif text-gray-300">
         “
       </div>
     </div>
